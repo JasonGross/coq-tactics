4be0b8fb2ccb3e2f9c30ee9fbcef9eff0c335f2e
Jason Gross <jasongross9@gmail.com>
2015-06-01 16:53:12 -0400

    Add [specialize_by]

--- Common.v
+++ Common.v
@@ -300,6 +300,16 @@ Ltac specialize_hyp_uniquely H :=
 Ltac specialize_all_uniquely := do_with_hyp specialize_hyp_uniquely.
 
 
+(** try to specialize all non-dependent hypotheses using [tac] *)
+Ltac specialize_by' tac := 
+  idtac;
+  match goal with
+    | [ H : ?A -> ?B |- _ ] => let H' := fresh in assert (H' : A) by tac; specialize (H H'); clear H'
+  end.
+
+Ltac specialize_by tac := repeat specialize_by' tac.
+
+
 (** Run [hnf] in anything inside of a [match] statement *)
 Ltac hnf_in_match' :=
   idtac;
